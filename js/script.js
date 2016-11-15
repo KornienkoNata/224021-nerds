df38e89e9f28160fea7159d8b545aaff19470e45
Natalia <port8080@mail.ru>
2016-11-15 19:43:08 +0300

    Еще раз

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 window.onload=function() {
   var link = document.querySelector(".map-contact .btn");
   var popup = document.querySelector(".feedback");
+  var inp_name = document.querySelector(".fullname");
+  var inp_email = document.querySelector(".email");
   var close = popup.querySelector(".feedback-close");
   var form = popup.querySelector("form");
   var fullname = popup.querySelector("[name=fullname]");
   var email = popup.querySelector("[name=email]");
   var storage = localStorage.getItem("fullname");
+  var storageemail = localStorage.getItem("email");
+  var message = popup.querySelector("[name=message]");
 
   link.addEventListener("click", function(event) {
     event.preventDefault();
@@ -15,24 +19,42 @@ window.onload=function() {
       email.focus();
       } else {
       fullname.focus();
+    };
+
+    if (storageemail) {
+      email.value = storageemail;
+      message.focus();
+      } else {
+      email.focus();
     }
+
   });
 
-  close.addEventListener("click", function(event) {
+    close.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.remove("feedback-show");
     popup.classList.remove("modal-error");
   });
 
   form.addEventListener("submit", function(event) {
-    if (!fullname.value || !email.value) {
+    if (!fullname.value) {
       event.preventDefault();
-      popup.classList.remove("modal-error");
-      popup.offsetWidth = popup.offsetWidth;
-      popup.classList.add("modal-error");
+      inp_name.classList.remove("modal-error");
+      inp_name.offsetWidth = inp_name.offsetWidth;
+      inp_name.classList.add("modal-error");
     } else {
       localStorage.setItem("fullname", fullname.value);
+    };
+
+     if (!email.value) {
+      event.preventDefault();
+      inp_email.classList.remove("modal-error");
+      inp_email.offsetWidth = inp_email.offsetWidth;
+      inp_email.classList.add("modal-error");
     }
+    else {
+      localStorage.setItem("email", email.value);
+    };
   });
 
   window.addEventListener("keydown", function(event) {
